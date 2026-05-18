3965cae224fafea70cdc0fbc0a9945b3179219a6
Md Tuhin <your_email@gmail.com>
2026-05-18 17:09:51 +0600

    Complete Mamun Automobiles ERP Backend Phase 1

--- routes/console.php
+++ routes/console.php
@@ -16,6 +16,10 @@ Artisan::command('git:push', function () {
     $output = shell_exec('git commit -m "Complete Mamun Automobiles ERP Backend Phase 1" 2>&1');
     $this->info($output);
 
+    $this->info('Renaming branch to main');
+    $output = shell_exec('git branch -M main 2>&1');
+    $this->info($output);
+
     $this->info('Running git push');
     $output = shell_exec('git push -u origin main 2>&1');
     $this->info($output);
